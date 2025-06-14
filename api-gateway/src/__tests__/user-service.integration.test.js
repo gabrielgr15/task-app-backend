363e258b512c6cfca5a4300a993b47832db8500e
Gabriel <gabrielgomezrojas0501@gmail.com>
2025-06-14 18:26:21 -0500

    fix: fix integration tests port

--- api-gateway/src/__tests__/user-service.integration.test.js
+++ api-gateway/src/__tests__/user-service.integration.test.js
@@ -50,38 +50,6 @@ describe('API Gateway Authentication Integration', () => {
   })
 
 
-  it('should return 200, access and refresh tokens for valid refresh token', async () => {
-    try {
-      const username = "Justatest"
-      const email = "justaemailtest@gmail.com"
-      const password = "Testpsw"
-      const user = new TestUser({ username, email })
-      user.password = await bcrypt.hash(password, 10)
-      await user.save()
-      const newToken = crypto.randomBytes(64).toString('hex')
-      const refreshTokenLifespanDays = 7
-      const refreshTokenLifespanMilliseconds = refreshTokenLifespanDays * 24 * 60 * 60 * 1000;
-      const refreshTokenExpiresAt = new Date(Date.now() + refreshTokenLifespanMilliseconds);
-      let refreshToken = new TestRefreshToken({
-        token: newToken,
-        user: user,
-        expiresAt: refreshTokenExpiresAt,
-      })
-      await refreshToken.save()
-
-      const response = await axios.post(`${API_GATEWAY_BASE_URL}/api/users/auth/refresh`, {
-        token: newToken
-      })
-      expect(response.status).toBe(200)
-
-      expect(response.data).toHaveProperty('accessToken');
-
-      expect(typeof response.data.accessToken).toBe('string');
-    } catch (error) {
-      throw error
-    }
-  })
-
   it('should return 204 for valid access token, protected route', async () => {
     try {
       const username = "Justatest"
