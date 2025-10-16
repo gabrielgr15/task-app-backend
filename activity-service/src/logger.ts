021486793fb391fb0e645c65cd6d344acc32a2cc
Gabriel <gabrielgomezrojas0501@gmail.com>
2025-10-16 00:43:50 -0500

    fix: edit winston file

--- activity-service/src/logger.ts
+++ activity-service/src/logger.ts
@@ -3,47 +3,33 @@ import 'winston-daily-rotate-file'
 import path from 'path'
 import fs from 'fs'
 
-
 const logsDirectory = path.join(__dirname, 'logs')
 if (!fs.existsSync(logsDirectory)) {
     fs.mkdirSync(logsDirectory);
 }
 
-const fileRotateTransport = new winston.transports.DailyRotateFile({
-    filename: path.join(logsDirectory, 'app-%DATE%.log'),
-    datePattern: 'YYYY-MM-DD',
-    zippedArchive: true,
-    maxSize: '20m',
-    maxFiles: '7d', 
-    level: 'debug'
-})
-
-const consoleTransport = new winston.transports.Console({
-    level: 'debug',
-})
-
 type Replacer = (key: string, value: any) => any
 
-const errorReplacer: Replacer = (key: string, value: any): any => {    
-    if (value instanceof Error) {        
+const errorReplacer: Replacer = (key: string, value: any): any => {
+    if (value instanceof Error) {
         const error: Record<string, any> = {}
-        
-        Object.getOwnPropertyNames(value).forEach((propName) => {            
+
+        Object.getOwnPropertyNames(value).forEach((propName) => {
             error[propName] = value[propName as keyof typeof value];
-        })        
+        })
         error.message = value.message;
         error.name = value.name;
-        error.stack = value.stack; 
-        
+        error.stack = value.stack;
+
         if (value.cause) {
-            try {                
+            try {
                 error.cause = JSON.parse(JSON.stringify(value.cause, errorReplacer))
             } catch (e) {
                 error.cause = "[Unserializable Cause]";
             }
         }
         return error
-    }    
+    }
     return value;
 }
 
@@ -51,15 +37,29 @@ const logger = winston.createLogger({
     level: 'debug',
     format: winston.format.combine(
         winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        winston.format.errors({ stack: true }), 
+        winston.format.errors({ stack: true }),
         winston.format.splat(),
-        winston.format.json({ replacer: errorReplacer, space: 2 }) 
-        
-    ),   
+        winston.format.json({ replacer: errorReplacer, space: 2 })
+
+    ),
     transports: [
-        fileRotateTransport,
-        consoleTransport 
+        new winston.transports.Console({
+            level: 'debug',
+        })
     ]
-})
+});
+
+// Only add file transport if not test env (unblocks CI perms drama)
+if (process.env.NODE_ENV !== 'test') {
+    const fileRotateTransport = new winston.transports.DailyRotateFile({
+        filename: path.join(logsDirectory, 'app-%DATE%.log'),
+        datePattern: 'YYYY-MM-DD',
+        zippedArchive: true,
+        maxSize: '20m',
+        maxFiles: '7d',
+        level: 'debug'
+    });
+    logger.add(fileRotateTransport);
+}
 
 export default logger
